5252587dc0f784832cf57af24bd0eadf1466f3a4
Andrey <flere2@hotmail.com>
2022-09-26 16:19:55 +0200

    commit tres, added roll

--- src/spil/Game.java
+++ src/spil/Game.java
@@ -1,4 +1,7 @@
 package spil;
 
 public class Game {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/spil/Rafflecup.java
@@ -0,0 +1,12 @@
+package spil;
+
+public class Rafflecup {
+
+
+    public Rafflecup(Dice d1, Dice d2){
+
+    }
+    public void shake(){
+
+    }
+}
