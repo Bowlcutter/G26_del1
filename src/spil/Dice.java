9e37f9cd7e95741b437266e2ea91af5e472391e0
Andrey <flere2@hotmail.com>
2022-09-26 13:13:48 +0200

    anden commit (tomme metoder + ændret til utf 8 encoding)

--- src/spil/Dice.java
+++ src/spil/Dice.java
@@ -1,4 +1,23 @@
 package spil;
 
 public class Dice {
+
+    private int faceValue;
+
+    public Dice(){
+
+    }
+
+    public void setDice(int d1, int d2){
+
+    }
+
+    public int getSum(){
+        return 1;
+    }
+
+    public boolean getEns(){
+        return true;
+    }
+
 }
