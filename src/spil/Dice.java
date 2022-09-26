fc23e2b923d0396429fdf0eec6be36e802c555ab
Andrey <flere2@hotmail.com>
2022-09-26 13:40:06 +0200

    commit tres, added roll

--- src/spil/Dice.java
+++ src/spil/Dice.java
@@ -2,17 +2,20 @@ package spil;
 
 public class Dice {
 
-    private int faceValue;
+    private int faceValue = 1;
 
     public Dice(){
 
+    }
+    public void roll(){
+        faceValue = (int)(Math.random()*6+1);
     }
 
     public void setDice(int d1, int d2){
 
     }
 
-    public int getSum(){
+    public int getSum(Dice d1, Dice d2){
         return 1;
     }
 
