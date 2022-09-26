40748b2a0373a295c12ccb8b401332bddf3d48f9
Andrey <flere2@hotmail.com>
2022-09-26 13:47:30 +0200

    commit 4

--- src/spil/Dice.java
+++ src/spil/Dice.java
@@ -2,21 +2,24 @@ package spil;
 
 public class Dice {
 
-    private int faceValue = 1;
+    private int faceValue1 = 1, faceValue2 = 1;
 
     public Dice(){
 
     }
+
     public void roll(){
-        faceValue = (int)(Math.random()*6+1);
+        faceValue1 = (int)(Math.random()*6+1);
+        faceValue2 = (int)(Math.random()*6+1);
     }
 
     public void setDice(int d1, int d2){
-
+        faceValue1 = d1;
+        faceValue2 = d2;
     }
 
-    public int getSum(Dice d1, Dice d2){
-        return 1;
+    public int getSum(){
+        return faceValue1+faceValue2;
     }
 
     public boolean getEns(){
